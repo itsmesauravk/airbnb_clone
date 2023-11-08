a7d261c7fe16033e900ef8109cd2cb72126dc282
itsmesauravk <sauravkarki10.12@gmail.com>
2023-11-08 21:55:54 +0545

    started serverside login/register

---
+++ api/index.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res) => {
+  res.json("test okk")
+})
+
+app.listen(4000, console.log(`Server is listning to port 4000`))
