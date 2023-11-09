03ccc9ecf4502d4a18d4b7d4add6ba32670e987e
itsmesauravk <sauravkarki10.12@gmail.com>
2023-11-09 11:35:53 +0545

    continue serverside login/register

--- api/index.js
+++ api/index.js
@@ -1,8 +1,18 @@
 const express = require("express")
+const cors = require("cors")
 const app = express()
 
-app.get("/test", (req, res) => {
-  res.json("test okk")
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+)
+app.use(express.json())
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body
+  res.json({ name, email, password })
 })
 
 app.listen(4000, console.log(`Server is listning to port 4000`))
