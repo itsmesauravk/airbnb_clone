fb35b2c46bea0e9c79104b503d3558ab7f768429
itsmesauravk <sauravkarki10.12@gmail.com>
2023-11-09 09:20:18 +0545

    continue serverside login/register

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,14 +1,38 @@
+import { useState } from "react"
 import { Link } from "react-router-dom"
+import axios from "axios"
 
 export default function RegisterPage() {
+  const [name, setName] = useState("")
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
+  function registerUser(ev) {
+    ev.preventDefault()
+    axios.get("http://localhost:4000/test")
+  }
   return (
     <div className="mt-2 grow flex items-center justify-around">
       <div className="mb-64">
         <h1 className="mb-2 text-4xl text-center">Register</h1>
-        <form className="max-w-xl mx-auto ">
-          <input type="text" placeholder="Saurav Karki" />
-          <input type="email" placeholder="your@email.com" />
-          <input type="password" placeholder="password" />
+        <form className="max-w-xl mx-auto " onSubmit={registerUser}>
+          <input
+            type="text"
+            placeholder="Saurav Karki"
+            value={name}
+            onChange={ev => setName(ev.target.value)}
+          />
+          <input
+            type="email"
+            placeholder="your@email.com"
+            value={email}
+            onChange={ev => setEmail(ev.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="password"
+            value={password}
+            onChange={ev => setPassword(ev.target.value)}
+          />
           <button className="primary text-white">Register</button>
           <div className="text-center text-gray-500 mt-1">
             Already have account?
